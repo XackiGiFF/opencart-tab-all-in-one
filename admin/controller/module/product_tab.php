7ef65bca4c4cf53b7da7a367a448cb6b124838d6
XackiGiFF <m.girsanoff@gmail.com>
2023-01-24 00:46:13 +0300

    from 2.0 to 3.0

--- admin/controller/module/product_tab.php
+++ admin/controller/module/product_tab.php
@@ -7,20 +7,20 @@ class ControllerModuleProductTab extends Controller {
 
     $this->document->setTitle($this->language->get('heading_title'));
     
-    $this->load->model('extension/module');
+    $this->load->model('setting/module');
 
     if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
       if (!isset($this->request->get['module_id'])) {
-        $this->model_extension_module->addModule('product_tab', $this->request->post);
+        $this->model_setting_module->addModule('product_tab', $this->request->post);
       } else {
-        $this->model_extension_module->editModule($this->request->get['module_id'], $this->request->post);
+        $this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
       }
 
       $this->cache->delete('product');
 
       $this->session->data['success'] = $this->language->get('text_success');
 
-      $this->response->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
+      $this->response->redirect($this->url->link('marketplace/module', 'user_token=' . $this->session->data['user_token'], 'SSL'));
     }
 
     $data['heading_title'] = $this->language->get('heading_title');
@@ -66,36 +66,36 @@ class ControllerModuleProductTab extends Controller {
 
     $data['breadcrumbs'][] = array(
       'text' => $this->language->get('text_home'),
-      'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
+      'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], 'SSL')
     );
 
     $data['breadcrumbs'][] = array(
       'text' => $this->language->get('text_module'),
-      'href' => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL')
+      'href' => $this->url->link('extension/module', 'user_token=' . $this->session->data['user_token'], 'SSL')
     );
 
     if (!isset($this->request->get['module_id'])) {
       $data['breadcrumbs'][] = array(
         'text' => $this->language->get('heading_title'),
-        'href' => $this->url->link('module/product_tab', 'token=' . $this->session->data['token'], 'SSL')
+        'href' => $this->url->link('module/product_tab', 'user_token=' . $this->session->data['user_token'], 'SSL')
       );
     } else {
       $data['breadcrumbs'][] = array(
         'text' => $this->language->get('heading_title'),
-        'href' => $this->url->link('module/product_tab', 'token=' . $this->session->data['token'] . '&module_id=' . $this->request->get['module_id'], 'SSL')
+        'href' => $this->url->link('module/product_tab', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], 'SSL')
       );
     }
 
     if (!isset($this->request->get['module_id'])) {
-      $data['action'] = $this->url->link('module/product_tab', 'token=' . $this->session->data['token'], 'SSL');
+      $data['action'] = $this->url->link('module/product_tab', 'user_token=' . $this->session->data['user_token'], 'SSL');
     } else {
-      $data['action'] = $this->url->link('module/product_tab', 'token=' . $this->session->data['token'] . '&module_id=' . $this->request->get['module_id'], 'SSL');
+      $data['action'] = $this->url->link('module/product_tab', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], 'SSL');
     }
 
-    $data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+    $data['cancel'] = $this->url->link('extension/module', 'user_token=' . $this->session->data['user_token'], 'SSL');
 
     if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
-      $module_info = $this->model_extension_module->getModule($this->request->get['module_id']);
+      $module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
     }
     
     if (isset($this->request->post['name'])) {
